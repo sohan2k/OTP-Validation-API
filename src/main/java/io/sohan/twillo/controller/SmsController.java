dcb4fc09ac81b54a95b314510d79803b37931a97
Sohan Barman <barmansohan9@mail.com>
2022-07-07 03:11:59 +0530

    controller added

--- src/main/java/io/sohan/twillo/controller/SmsController.java
+++ src/main/java/io/sohan/twillo/controller/SmsController.java
@@ -1,10 +1,8 @@
 package io.sohan.twillo.controller;
 
-import io.sohan.twillo.configuration.SmsService;
-import io.sohan.twillo.entites.GeneratedOtp;
-import io.sohan.twillo.entites.Otp;
+import io.sohan.twillo.dto.OtpDto;
 import io.sohan.twillo.entites.User;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.sohan.twillo.service.SmsService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,28 +11,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SmsController {
-    @Autowired
-    private SmsService smsService;
+    private final SmsService smsService;
+
+    public SmsController(SmsService smsService) {
+        this.smsService = smsService;
+    }
 
     @PostMapping("/send")
     public ResponseEntity<String> sendOtp(@RequestBody User user) {
-        try {
-            smsService.send(user);
-            System.out.println(user.getPhoneNo());
-            return new ResponseEntity<>("Otp send Successfully ", HttpStatus.OK);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-//        smsService.send(user);
-//        System.out.println(user.getPhoneNo());
-//        return "Otp send Successfully ";
+            if(smsService.send(user)){
+                System.out.println(user.getPhoneNo());
+                return new ResponseEntity<>("Otp send Successfully ", HttpStatus.OK);
+            }
         return new ResponseEntity<>("failure ", HttpStatus.UNAUTHORIZED);
     }
     @PostMapping("/validate")
-    public ResponseEntity<String> validateOtp(@RequestBody Otp otp){
-        int generatedOtp=GeneratedOtp.getOtp();
-        //System.out.println(otp.getOtp());
-        if(otp.getOtp()== generatedOtp)
+    public ResponseEntity<String> validateOtp(@RequestBody OtpDto otpDto){
+        if(smsService.isValid(otpDto))
             return new ResponseEntity<>("Correct OTP", HttpStatus.OK);
         return new ResponseEntity<>("Wrong OTP ", HttpStatus.UNAUTHORIZED);
     }
