1cda74d720254581bbc263731f22e3e353f0f502
Sohan Barman <barmansohan9@mail.com>
2022-07-07 03:10:24 +0530

    otp dto added

---
+++ src/main/java/io/sohan/twillo/dto/OtpDto.java
@@ -0,0 +1,14 @@
+package io.sohan.twillo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OtpDto {
+    private int otp;
+}
