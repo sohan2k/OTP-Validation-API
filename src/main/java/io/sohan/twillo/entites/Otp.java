9b574be69d9b723d77843499a66f0b002abc3cdb
Sohan Barman <barmansohan9@mail.com>
2022-07-07 03:10:08 +0530

    otp entity

--- src/main/java/io/sohan/twillo/entites/Otp.java
+++ src/main/java/io/sohan/twillo/entites/Otp.java
@@ -1,13 +1,36 @@
 package io.sohan.twillo.entites;
 
+import io.sohan.twillo.entites.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class Otp {
-    int otp;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
-    public int getOtp() {
-        return otp;
+    @Builder.Default
+    private int otp=(int) (Math.random()*(99999-10000)+10000);
+    private String phoneNo;
+    //String msg;
+    private Date expiryTime;//=new Date(System.currentTimeMillis()+12000);
+
+    @Builder.Default
+    @Enumerated(value = EnumType.STRING)
+    private Status status=Status.Failed;
+    public void generate(String phoneNo){
+        this.phoneNo=phoneNo;
+        //this.msg="Your OTP is:"+ phoneNo +" for test spring message api";
+        this.expiryTime=new Date(System.currentTimeMillis()+12000);
     }
 
-    public void setOtp(int otp) {
-        this.otp = otp;
-    }
 }
